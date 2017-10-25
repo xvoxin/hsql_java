1b144aa3ac521dbd85a13e59a3c942f57f8ed9b3
Michał Niskowski <niski315@gmail.com>
2017-10-25 19:48:17 +0200

    change scripts to bat and change to intellij project

--- db/runHSQLDBClient.bat
+++ db/runHSQLDBClient.bat
@@ -1,3 +1,2 @@
-#!/bin/sh
 
 java -cp ./hsqldb.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/workdb
--- db/runHSQLDBServer.bat
+++ db/runHSQLDBServer.bat
@@ -1,3 +1,2 @@
-#!/bin/bash
 
 java -cp ./hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
